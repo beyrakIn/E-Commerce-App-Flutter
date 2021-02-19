596cb431570c2acfac6e320273c62a25341a71e3
B E Y R ∆ K <68893229+beyrakIn@users.noreply.github.com>
2021-02-19 23:08:44 +0400

    Profile

--- lib/screens/fragments/profile.dart
+++ lib/screens/fragments/profile.dart
@@ -23,30 +23,50 @@ class Profile extends StatelessWidget {
               backgroundColor: Colors.red,
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: FlatButton(
-              padding: EdgeInsets.all(20),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15)),
-              color: Colors.white54,
-              onPressed: () {},
-              child: Row(
-                children: [
-                  Icon(Icons.verified_user),
-                  SizedBox(width: 20),
-                  Expanded(
-                    child: Text(
-                      "My Account",
-                      style: Theme.of(context).textTheme.bodyText1,
-                    ),
-                  ),
-                  Icon(Icons.arrow_forward_ios)
-                ],
+          ProfileMenu(text: "My Account", icon: Icon(Icons.verified_user)),
+          ProfileMenu(text: "My Account", icon: Icon(Icons.verified_user)),
+          ProfileMenu(text: "My Account", icon: Icon(Icons.verified_user)),
+          ProfileMenu(text: "My Account", icon: Icon(Icons.verified_user)),
+        ],
+      ),
+    );
+  }
+}
+
+class ProfileMenu extends StatelessWidget {
+  final String text;
+  final Icon icon;
+  final VoidCallback press;
+
+  ProfileMenu({
+    Key key,
+    this.text,
+    this.press,
+    this.icon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: FlatButton(
+        padding: EdgeInsets.all(20),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        color: Colors.white54,
+        onPressed: () {},
+        child: Row(
+          children: [
+            icon,
+            SizedBox(width: 20),
+            Expanded(
+              child: Text(
+                text,
+                style: Theme.of(context).textTheme.bodyText1,
               ),
             ),
-          )
-        ],
+            Icon(Icons.arrow_forward_ios)
+          ],
+        ),
       ),
     );
   }
