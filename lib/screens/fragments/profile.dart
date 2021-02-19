f1aa9495af48f353c34742fb5fdf3109efbb07fe
B E Y R ∆ K <68893229+beyrakIn@users.noreply.github.com>
2021-02-19 22:43:08 +0400

    Profile

--- lib/screens/fragments/profile.dart
+++ lib/screens/fragments/profile.dart
@@ -8,7 +8,45 @@ class Profile extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        title: Text("Profile"),
+        title: Text(
+          "Profile",
+          textAlign: TextAlign.center,
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: 115,
+            height: 115,
+            child: CircleAvatar(
+              backgroundColor: Colors.red,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: FlatButton(
+              padding: EdgeInsets.all(20),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15)),
+              color: Colors.white54,
+              onPressed: () {},
+              child: Row(
+                children: [
+                  Icon(Icons.verified_user),
+                  SizedBox(width: 20),
+                  Expanded(
+                    child: Text(
+                      "My Account",
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                  ),
+                  Icon(Icons.arrow_forward_ios)
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
