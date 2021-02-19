ae5210db7353e48603805d6499f8a5d75e4a8205
B E Y R ∆ K <68893229+beyrakIn@users.noreply.github.com>
2021-02-19 22:21:55 +0400

    Profile

--- lib/screens/fragments/profile.dart
+++ lib/screens/fragments/profile.dart
@@ -4,7 +4,10 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.teal,
       appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
         title: Text("Profile"),
       ),
     );
